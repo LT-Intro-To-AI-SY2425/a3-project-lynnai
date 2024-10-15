bc48cdda2fbac4bf10a88260f965a9974a2cc38e
lyogics <lyogi@cps.edu>
2024-10-15 11:27:43 -0500

    completed the games database!!!!!!

---
+++ games.py
@@ -0,0 +1,82 @@
+from typing import List, Tuple
+
+game_db: List[Tuple[str, str, str, int]] = [
+    (
+        "pokemon", # title
+        "game freak", # developer/dev company
+        "role-playing game", # genre
+        1996, # year game came out (or 1st game, if series)
+    ),
+    (
+        "minecraft",
+        "mojang",
+        "sandbox",
+        2009,
+    ),
+    (
+        "warface",
+        "crytek",
+        "first-person shooter",
+        2013,
+    ),
+    (
+        "rhythm heaven",
+        "tnx",
+        "rhythm",
+        2006,
+    ),
+    (
+        "beat saber",
+        "beat games",
+        "virtual reality",
+        2018,
+    ),
+    (
+        "cuphead",
+        "studio mdhr",
+        "platformer",
+        2017,
+    ),
+    (
+        "battlefield",
+        "dice",
+        "first-person shooter",
+        2002,
+    ),
+    (
+        "super meat boy",
+        "team meat",
+        "platformer",
+        2010,
+    ),
+    (
+        "five nights at freddy's",
+        "scott cawthon",
+        "horror",
+        2014,
+    ),
+    (
+        "emily wants to play",
+        "shawn hitchcock",
+        "horror",
+        2015,
+    ),
+    (
+        "garry's mod",
+        "facepunch studios",
+        "sandbox",
+        2006,
+    ),
+    (
+        "skyrim",
+        "bethesda game studios",
+        "role-playing game",
+        2011,
+    ),
+    (
+        "doki doki literature club",
+        "team salvato",
+        "visual novel",
+        2017,
+    ),  
+] 
